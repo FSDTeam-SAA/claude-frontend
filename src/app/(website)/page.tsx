ae48bde229cd6986dcd62548c419dbe747896a36
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-12-13 16:59:45 +0600

    faq section design fix up

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 // import { MissionSection } from './_components/mission-section'
-import { FAQSection } from './_components/faq-section'
 import HeroSection from './_components/hero-section'
 import OurPartners from './_components/our-partners'
+import { FaqSection } from './_components/faq-section'
 
 const HomePage = () => {
     return (
@@ -15,7 +15,7 @@ const HomePage = () => {
                 <AnalyticsProcessSection />
                 <PricingSection /> */}
                 <OurPartners/>
-                <FAQSection />
+                <FaqSection />
             </>
         </div>
     )
