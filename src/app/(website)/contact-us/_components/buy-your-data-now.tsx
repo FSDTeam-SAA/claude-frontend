0527c342e5631a1cb5ef32978f74916e0f41e01d
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-01-04 12:34:49 +0600

    problem fix up

--- src/app/(website)/contact-us/_components/buy-your-data-now.tsx
+++ src/app/(website)/contact-us/_components/buy-your-data-now.tsx
@@ -12,7 +12,7 @@ const BuyYourDataNow = () => {
         <div className="w-full flex flex-col items-center justify-center">
           <p className='w-full md:max-w-2xl text-lg md:text-xl text-[#333333] font-medium leading-[150%] text-center py-4 md:py-6 lg:py-8'><span className='text-primary'>Analytic Soccer</span> is the first data platform designed for amateur players. Showcase your profile, upload your stats, get rated, and track your transfer history <span className='text-primary'>just like the pros.</span></p>
           <Link href="#">
-          <button className='h-[56px] px-6 md:px-12 lg:px-16 text-base leading-[120%] text-white font-bold bg-primary rounded-[8px]'>Buy Your Data Now</button>
+          <button className='h-[48px] md:h-[52px] lg:h-[56px] px-6 md:px-12 lg:px-16 text-sm md:text-base leading-[120%] text-white font-bold bg-primary rounded-[8px]'>Buy Your Data Now</button>
           </Link>
         </div>
       </div>
