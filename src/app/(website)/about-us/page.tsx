086c9ec82f2b1227ccfe777b75e677b2afc3064f
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-01-05 09:05:53 +0600

    contact us problem fix up

--- src/app/(website)/about-us/page.tsx
+++ src/app/(website)/about-us/page.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import OurGrowthStory from './_components/our-growth-story'
-import MeetOurCertified from './_components/meet-our-certified'
-import AnalyticSoccer from './_components/analytic-soccer'
-import OurPromiseToYou from './_components/our-promise-to-you'
-import ActivateYourProfile from '../contact-us/_components/activate-your-profile'
-import MeetOurAnalyticsMember from './_components/meet-our-analytics-member'
+// import OurGrowthStory from './_components/our-growth-story'
+// import MeetOurCertified from './_components/meet-our-certified'
+// import AnalyticSoccer from './_components/analytic-soccer'
+// import OurPromiseToYou from './_components/our-promise-to-you'
+// import ActivateYourProfile from '../contact-us/_components/activate-your-profile'
+// import MeetOurAnalyticsMember from './_components/meet-our-analytics-member'
+import BuyYourDataNow from './_components/buy-your-data-now'
+import ProjectLeader from './_components/project-leaders'
 
 const AboutUsPage = () => {
   return (
     <div>
-      <AnalyticSoccer/>
+      {/* <AnalyticSoccer/>
       <OurGrowthStory/>
       <ActivateYourProfile/>
       <MeetOurAnalyticsMember/>
       <MeetOurCertified/>
-      <OurPromiseToYou/>
+      <OurPromiseToYou/> */}
+
+        <ProjectLeader/>
+      <BuyYourDataNow/>
     </div>
   )
 }
